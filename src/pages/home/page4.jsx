9a465ff8bde76af0f7c0b20bca60b8bf84333514
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-02-25 13:23:19 -0600

    page 4

--- src/pages/home/page4.jsx
+++ src/pages/home/page4.jsx
@@ -1,7 +1,43 @@
+import background from "../../assets/bg-home4.jpg";
+import packages from "../../assets/icon-package.png";
+import money from "../../assets/icon-money.png";
+import tablet from "../../assets/icon-tablet.png";
+
 export default function Page4() {
-    return (
-        <div className="vh-100" id="page4">
-            
-        </div>
-    );
+	return (
+		<div className="segoe">
+			<div
+				className="background px-2 px-md-3 py-2 py-lg-5 text-white text-center"
+				style={{ backgroundImage: `url(${background})` }}
+			>
+				<div className="mt-5 mb-1 display-5 fw-bold">LCD Buyback</div>
+				<div className="h6 fw-light mt-3 mb-5">
+					Sell your broken screen to us
+				</div>
+				<div className="h6 fw-light text-decoration-underline mb-5">
+					learn more
+				</div>
+			</div>
+			<div className="d-flex flex-column flex-md-row justify-content-center align-items-center p-5 text-center">
+				<div className="m-2 m-lg-5">
+					<div className="buyback-icon p-2">
+						<img className="position-absolute top-50 start-50 translate-middle" src={packages} alt="packages"/>
+					</div>
+					<div className="h5 fw-light">Mail your screen to us</div>
+				</div>
+				<div className="m-2 m-lg-5">
+					<div className="buyback-icon">
+						<img className="position-absolute top-50 start-50 translate-middle" src={tablet} alt="tablet" />
+					</div>
+					<div className="h5 fw-light">We evalue your screen</div>
+				</div>
+				<div className="m-2 m-lg-5">
+					<div className="buyback-icon">
+						<img className="position-absolute top-50 start-50 translate-middle" src={money} alt="payout" />
+					</div>
+					<div className="h5 fw-light">Get paid in Cash, Paypal or Cash App</div>
+				</div>
+			</div>
+		</div>
+	);
 }
