9176de563d754dcd369fa30e4599c3d47fc36441
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-03-18 15:25:27 -0500

    about us page

--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -4,38 +4,53 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 export default function NavBar() {
+	const [navHeight, setNavHeight] = useState(0);
 	const navRef = useRef();
+
 	useEffect(() => {
 		//hide scrollbar when scrolling down, show it when scrolling back up
 		let lastScroll = window.pageYOffset;
 		const handleScroll = () => {
-			console.log("scrolling??", scroll, lastScroll);
 			const scroll = window.pageYOffset;
 
-			if(lastScroll > scroll) {
-				navRef.current.style.top="0";
+			if (scroll > 0) {
+				navRef.current.style.position = "fixed";
 			} else {
-				navRef.current.style.top=  `-${navRef.current.clientHeight}px`;
+				navRef.current.style.position = "relative";
+			}
+
+			if (lastScroll > scroll || scroll < navRef.current.clientHeight) {
+				navRef.current.style.top = 0;
+			} else {
+				navRef.current.style.top = `${-navRef.current.clientHeight}px`;
 			}
 
 			lastScroll = scroll;
 		};
 		window.addEventListener("scroll", handleScroll);
 
-		return window.removeEventListener("scroll", handleScroll);
-	}, [])
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
+
+	useEffect(() => {
+		setNavHeight(navRef.current.clientHeight);
+	})
 
 	return (
-		<div className="nav-bar" ref={navRef}>
-            <Link to={{pathname: "/"}}>
-                <img src={logo} alt="lenwell"/>
-            </Link>
-			<div className="nav-link-list">
-				<Link to={{pathname: "/Repair Service"}}>Repair Service</Link>
-				<Link to={{pathname: "/lcd-swap-service"}}>LCD Swap Service</Link>
-				<Link to={{pathname: "/lcd-buyback"}}>LCD Buyback</Link>
-				<Link to={{pathname: "/about-us"}}>About us</Link>
-				<Link to={{pathname: "/contact-us"}}>Contact us</Link>
+		<div id="caasdasd" style={{minHeight: navHeight}}>
+			<div className="nav-bar" ref={navRef}>
+				<Link to={{ pathname: "/" }}>
+					<img src={logo} alt="lenwell" />
+				</Link>
+				<div className="nav-link-list">
+					<Link to={{ pathname: "/Repair Service" }}>Repair Service</Link>
+					<Link to={{ pathname: "/lcd-swap-service" }}>LCD Swap Service</Link>
+					<Link to={{ pathname: "/lcd-buyback" }}>LCD Buyback</Link>
+					<Link to={{ pathname: "/about-us" }}>About us</Link>
+					<Link to={{ pathname: "/contact-us" }}>Contact us</Link>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -1,9 +1,77 @@
 import "./style.css";
+import tag from "../../assets/google-maps.png";
+import { Link } from "react-router-dom";
 
 export default function AboutUs() {
-    return (
-        <div>
-            the about us page
-        </div>
-    )
+	return (
+		<div>
+			<div className="display-6 m-3 border-bottom w-fit-content">About us</div>
+			<div className="h3 text-center mt-5 border-bottom p-1">
+				Lenwell Houston Location
+			</div>
+			<div className="text-center m-4">
+				<div className="text-muted">5750 Bintliff Dr Ste 210</div>
+				<div className="text-muted">Houston, Texas, 77036</div>
+				<div className="text-muted">(832) 275-5999</div>
+			</div>
+			<div className="m-5"></div>
+			<div className="d-flex justify-content-center">
+				<a
+					className="btn btn-dark direction-button"
+					href={`https://goo.gl/maps/NYGrZTB8EGiRn8ug7`}
+				>
+					<img src={tag} alt="direction" />
+					Direction
+				</a>
+			</div>
+			<div className="h3 text-center mt-5 border-bottom p-1">
+				Business Hours
+			</div>
+			<div className="m-4"></div>
+			<div className="h5 text-center fw-light">
+				We are OPEN now, <br />
+				We close at 7:00pm today
+			</div>
+			<div className="mx-auto w-fit-content">
+				<table className="business-hour my-4">
+					<tbody>
+						<tr>
+							<td>Monday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Tuesday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Wednesday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Thursday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Friday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Saturday:</td>
+							<td>9:00AM ~ 7:00PM</td>
+						</tr>
+						<tr>
+							<td>Sunday:</td>
+							<td>Closed</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div className="text-center m-5">
+				<div className="h6">Have a question?</div>
+				<Link to="/contact-us" className="button button-primary">
+					Contact us
+				</Link>
+			</div>
+		</div>
+	);
 }
