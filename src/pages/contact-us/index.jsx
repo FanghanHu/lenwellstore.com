09240e13499521f04716da87e72942d17295f8da
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-03-22 18:03:40 -0500

    email form

--- src/pages/contact-us/index.jsx
+++ src/pages/contact-us/index.jsx
@@ -2,9 +2,33 @@ import "./style.css";
 import banner from "../../assets/bg-home5.jpg";
 import GlassPanel from "../../component/glass-panel";
 import DirectionButton from "../../component/direction-button";
+import { useCallback, useState } from "react";
+import axios from "axios";
+
+const axiosIns = axios.create({
+    transformRequest: (data, headers) => {
+        return JSON.stringify(data);
+    }
+});
 
 export default function ContactUs() {
     //send the contact us form
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [isSending, setIsSending] = useState(false);
+
+    const sendMessage = () => {
+        setIsSending(true);
+        axiosIns.post("https://hooks.zapier.com/hooks/catch/9413593/bmgsqx8/", {
+            email: email,
+            message: message
+        }).then(res => {
+            setMessage("");
+            setIsSending(false);
+            alert("Message received, thank you!");
+        });
+    }
+
     return (
         <div>
             <div className="display-6 m-3 border-bottom w-fit-content">Contact Us</div>
@@ -30,14 +54,18 @@ export default function ContactUs() {
             <div className="mx-auto mb-4" style={{width: "max(350px, min(60%, 700px))"}}>
                 <div className="mb-3">
                     <label className="form-label fw-light">Email:</label>
-                    <input type="email" className="form-control" placeholder="name@example.com"/>
+                    <input type="email" className="form-control" placeholder="name@example.com" value={email} onChange={(e)=> {
+                            setEmail(e.target.value);
+                    }} />
                 </div>
                 <div className="mb-3">
                     <label className="form-label fw-light">Message: </label>
-                    <textarea className="form-control" rows="5"></textarea>
+                    <textarea className="form-control" rows="5" value={message} onChange={(e)=> {
+                            setMessage(e.target.value);
+                    }} ></textarea>
                 </div>
                 <div className="d-flex justify-content-end">
-                    <div className="button button-primary">Send</div>
+                    <button className="button button-primary" onClick={sendMessage} disabled={isSending}>Send</button>
                 </div>
             </div>
         </div>
