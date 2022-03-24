d067ef775aa1d64d56c5ed460f7820ca5953d792
Fanghan Hu <Fanghan.Hu@gmail.com>
2022-03-24 18:43:55 -0500

    adjusting parallax for best performance

--- src/component/effects/mouse-parallax/index.jsx
+++ src/component/effects/mouse-parallax/index.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef } from "react";
 import "./style.css";
 
 //time between each style assignment, in ms
-const COOL_DOWN = 10;
+const COOL_DOWN = 30;
 
 export default function MouseParallax({image, imageScale=110, isReverse=false, style, className=""}) {
     const ref = useRef();
@@ -14,13 +14,15 @@ export default function MouseParallax({image, imageScale=110, isReverse=false, s
             
             //timeRef adds a cool down timer to the effect, the numbebr is in seconds
             if(now - timeRef.current > COOL_DOWN) {
-                const left = (e.x / window.innerWidth * (imageScale - 100)) - (imageScale - 100);
-                const top = (e.y / window.innerHeight * (imageScale - 100)) - (imageScale - 100);
-                // //console.log(`top: ${top}, left: ${left}`);
-                // ref.current.style.top = (isReverse ? -(top + (imageScale - 100)) : top) + "%";
-                // ref.current.style.left = (isReverse ? -(left + (imageScale - 100)) : left) + "%";
-                ref.current.style.setProperty("--top", (isReverse ? -(top + (imageScale - 100)) : top) + "%");
-                ref.current.style.setProperty("--left", (isReverse ? -(left + (imageScale - 100)) : left) + "%");
+                let x = (e.x / window.innerWidth - 0.5) * (imageScale - 100) / imageScale * 100;
+                let y = (e.y / window.innerHeight - 0.5) * (imageScale - 100) / imageScale * 100;
+
+                if(isReverse) {
+                    x = -x;
+                    y = -y;
+                }
+
+                ref.current.style.transform = `translate(${x}%, ${y}%)`;
                 timeRef.current = now;
             }
         }
@@ -36,8 +38,8 @@ export default function MouseParallax({image, imageScale=110, isReverse=false, s
         <img src={image} alt="parallax" ref={ref} className={`parallax ${className}`} style={{
             width: imageScale + "%",
             height: imageScale + "%",
-            "--top": -(imageScale-100)/2 + "%",
-            "--left": -(imageScale-100)/2 + "%",
+            top: -(imageScale - 100)/2 + "%",
+            left: -(imageScale - 100)/2 + "%",
             ...style
         }}/>
     );
